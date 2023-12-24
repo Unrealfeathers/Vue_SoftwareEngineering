3d992839de5f8f9ce7c0d316f8e3705a043496b3
Unrealfeathers <Unrealfeathers@outlook.com>
2023-12-24 22:46:26 +0800

    新增登陆界面

--- README.md
+++ README.md
@@ -1,14 +1,6 @@
 # Vue_SoftwareEngineering
 
-This template should help get you started developing with Vue 3 in Vite.
-
-## Recommended IDE Setup
-
-[VSCode](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur) + [TypeScript Vue Plugin (Volar)](https://marketplace.visualstudio.com/items?itemName=Vue.vscode-typescript-vue-plugin).
-
-## Customize configuration
-
-See [Vite Configuration Reference](https://vitejs.dev/config/).
+基于Vue3的软件工程前端用户交互界面。  
 
 ## Project Setup
 
---
+++ src/views/Login.vue
@@ -0,0 +1,44 @@
+<script setup>
+import { User, Lock } from '@element-plus/icons-vue'
+import { ref } from 'vue'
+
+
+// 控制注册与登录表单的显示， 默认显示注册
+const isRegister = ref(false)
+
+</script>
+
+<template>
+    <div class="background">
+        <el-container>
+            <el-main>
+                <el-card class="login-wrapper">
+                    <template #header>
+                        <div class="login-header">
+                            <el-text class="header-text">
+                                登录
+                            </el-text>
+                        </div>
+                    </template>
+                    <input v-model="input_username" placeholder="用户名" class="input-item">
+                    <input v-model="input_password" placeholder="密码" class="input-item">
+                    <div class="button">登录</div>
+                    <template #footer>
+                        <div class="login-footer">
+                            <el-text class="footer-text">
+                                没有账号？
+                            </el-text>
+                            <el-text class="footer-text-light" @click="isRegister = !isRegister">
+                                点击注册
+                            </el-text>
+                        </div>
+                    </template>
+                </el-card>
+            </el-main>
+        </el-container>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@import url(../assets/style/login.scss);
+</style>
