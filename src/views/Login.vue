45ab8001d5667e70d8c204426739a5a520bdf334
Unrealfeathers <Unrealfeathers@outlook.com>
2023-12-26 09:24:06 +0800

    完成登录注册界面

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,9 +2,8 @@
 import { User, Lock } from '@element-plus/icons-vue'
 import { ref } from 'vue'
 
-
-// 控制注册与登录表单的显示， 默认显示注册
-const isRegister = ref(false)
+// 控制注册与登录表单的显示， 默认显示登录
+const isLogin = ref(true)
 
 </script>
 
@@ -12,7 +11,7 @@ const isRegister = ref(false)
     <div class="background">
         <el-container>
             <el-main>
-                <el-card class="login-wrapper">
+                <el-card class="login-wrapper" v-if="isLogin">
                     <template #header>
                         <div class="login-header">
                             <el-text class="header-text">
@@ -28,9 +27,29 @@ const isRegister = ref(false)
                             <el-text class="footer-text">
                                 没有账号？
                             </el-text>
-                            <el-text class="footer-text-light" @click="isRegister = !isRegister">
+                            <el-link class="footer-text-light" type="info" :underline="false" @click="isLogin = false">
                                 点击注册
+                            </el-link>
+                        </div>
+                    </template>
+                </el-card>
+                <el-card class="login-wrapper" v-if="!isLogin">
+                    <template #header>
+                        <div class="login-header">
+                            <el-text class="header-text">
+                                注册
                             </el-text>
+                        </div>
+                    </template>
+                    <input v-model="input_username" placeholder="用户名" class="input-item">
+                    <input v-model="input_password" placeholder="密码" class="input-item">
+                    <input v-model="input_password" placeholder="重复密码" class="input-item">
+                    <div class="button">注册</div>
+                    <template #footer>
+                        <div class="login-footer">
+                            <el-link class="footer-text-light" type="info" :underline="false" @click="isLogin = true">
+                                ←返回
+                            </el-link>
                         </div>
                     </template>
                 </el-card>
